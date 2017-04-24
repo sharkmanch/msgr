72cf479c160f87eef3689fcf27d5aef163ab39cf
Shark Man <danteman1992@gmail.com>
2017-04-25 03:51:26 +0800

    get post update delete
    havent debug or test, try later

--- app.js
+++ app.js
@@ -5,11 +5,29 @@ const mongoose = require('mongoose');
 
 //get Msgrm
 const messenger_room = require('./models/messenger_room')
-
+//<3
 //mongoose connect
 mongoose.connect('mongodb://localhost/messenger');
 const db = mongoose.connection;
+//updated @ 3:24 25/4 ------------------------------------------------------
+/* problems here i dunno what is this when looking at bookstore, if you see it you may wanna tell me haha
 
+
+ app.all('*', function(req, res, next) {
+ res.header('Access-Control-Allow-Origin', '*');
+ res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+ res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
+ // intercept OPTIONS method
+ if ('OPTIONS' == req.method) {
+ res.send(200);
+ }
+ else {
+ next();
+ }
+ });
+
+
+ */
 //handling request with get put post delete (all kinds of http req)
 //so when ppl visit this '/' the function will run
 app.get('/', (req, res)=>{
@@ -25,6 +43,53 @@ app.get('/api/messenger_rooms', (req, res)=>{
     })
 });
 
+// get room by id:
+app.get('/api/messenger_rooms/:_id', (req, res)=>{
+   messenger_room.get_messenger_room_by_ID(req.params._id, (err, book)=>{
+       if(err){
+           throw err;
+       }
+       res.json(messenger_room);
+   })
+
+});
+
+//add/post room
+add.post('/api/messenger_rooms', (req,res)=>{
+    var messenger_room = req.body;
+    messenger_room.add_messenger_room(messenger_room, (err, messenger_room)=>{
+        if(err){
+            throw err;
+        }
+        res.json(messenger_room);
+    });
+});
+//update needed?
+/*
+app.put('/api/messenger_rooms/:_id', (req, res) =>{
+    var id = req.params._id;
+    var messenger_room = req.body;
+    messenger_room.update_messenger_room(id, messenger_room, {}, (err, messenger_room) =>{
+    if(err){
+        throw err;
+        }
+        res.json(messenger_room);
+    });
+});
+
+ */
+
+//delete room
+app.delete('/api/messenger_rooms/:_id', (req, res)=>{
+   var id = req.params._id;
+   messenger_room.remove_messsenger_room(id, (err, messenger_room)=>{
+       if(err){
+           throw err;
+       }
+       res.json(messenger_room);
+   });
+});
 
 app.listen(3009);
 console.log('Running on port 3009');
+//testing 123
--- models/messenger_room.js
+++ models/messenger_room.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+//newest update removed db collection - msgrms and replaced it with messenger_rooms for the updated react version.
 /**
  * messenger room schema
  */
@@ -23,3 +23,30 @@ module.exports.messenger_room = messenger_room;
 module.exports.get_messenger_rooms = (callback, limit) => {
     messenger_room.find(callback).limit(limit);
 };
+//--------------------------------------------------below = updated area 25/4 03:00am-------
+//get room by id
+module.exports.get_messenger_room_by_ID = (id, callback) =>{
+    messenger_room.findById(id, callback);
+}
+
+//post or add room .
+module.exports.add_messenger_room =  (messenger_room, callback) =>{
+    messenger_room.create(messenger_room, callback);
+}
+
+//i dont think updating room is necessary, probably updating the roomname or room no. for fun
+/*
+module.exports.update_messenger_room = (id, messenger_room, options, callback) =>{
+var query = {_id: id};
+var update = {
+       .......... things here to be confirmed.........
+    room_number: messenger_room.room_number
+}
+messenger_room.findOneAndUpdate(query, update, options, callback);
+
+ */
+//delete rooms
+module.exports.remove_messsenger_room = (id, callback) =>{
+    var query = {_id:id};
+    messenger_room.remove(query, callback);
+}
